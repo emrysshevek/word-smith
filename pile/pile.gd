7646a9c46c654cef1ceebc332a444e6a0f1a6942
Emrys Shevek <emrysshevek@gmail.com>
2026-05-25 16:15:34 -0700

    pile elemtn

--- pile/pile.gd
+++ pile/pile.gd
@@ -2,5 +2,25 @@ class_name Pile
 extends Control
 
 
-func space(count, elem_size:=Vector2.ZERO, max_offset:=INF, min_offset:=0.0) -> void:
-	pass
+func reposition_with_area(pos: Vector2, area: Vector2) -> void:
+	global_position = pos
+	size = area
+	
+
+func reposition_with_corners(corner1: Vector2, corner2: Vector2) -> void:
+	var top_left = Vector2(min(corner1.x, corner2.x), min(corner1.y, corner2.y))
+	var bot_right = Vector2(max(corner1.x, corner2.x), max(corner1.y, corner2.y))
+	
+	global_position = top_left
+	size = bot_right - top_left
+	
+	
+#region Input testing
+var start: Vector2
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("lmb"):
+		start = get_global_mouse_position()
+	if Input.is_action_pressed("lmb"):
+		reposition_with_corners(start, get_global_mouse_position())
+	
+#endregion
