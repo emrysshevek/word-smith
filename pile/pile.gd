ac6b7efd7c0eee4cdd710c86805d2909e0a4a929
Emrys Shevek <emrysshevek@gmail.com>
2026-05-28 11:20:23 -0700

    temp

--- pile/pile.gd
+++ pile/pile.gd
@@ -1,13 +1,55 @@
 class_name Pile
-extends Control
+extends Container
+
+#var _elem_props: Array[Dictionary]
+var elements: Array[Node]
+var count: int:
+	get:
+		return elements.size()
 
 
-func reposition_with_area(pos: Vector2, area: Vector2) -> void:
+func add_element(element: Node, index:=-1) -> void:
+	elements.insert(index, element)
+	queue_sort()
+
+
+func remove_element(element: Node) -> void:
+	elements.erase(element)
+	queue_sort()
+
+
+func shuffle_elements() -> void:
+	elements.shuffle()
+	queue_sort()
+
+
+func get_next_element(top:=true) -> Node:
+	queue_sort()
+	if top:
+		return elements.pop_front()
+	else:
+		return elements.pop_back()
+	
+
+func get_next_elements(count:int, top:=true) -> Array[Node]:
+	queue_sort()
+	return range(count).map(func(_i): return get_next_element(top))
+	
+	
+func _sort_elements() -> void:
+	if count == 0:
+		return
+		
+	
+	
+
+
+func _reposition_with_area(pos: Vector2, area: Vector2) -> void:
 	global_position = pos
 	size = area
 	
 
-func reposition_with_corners(corner1: Vector2, corner2: Vector2) -> void:
+func _reposition_with_corners(corner1: Vector2, corner2: Vector2) -> void:
 	var top_left = Vector2(min(corner1.x, corner2.x), min(corner1.y, corner2.y))
 	var bot_right = Vector2(max(corner1.x, corner2.x), max(corner1.y, corner2.y))
 	
@@ -21,6 +63,6 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("lmb"):
 		start = get_global_mouse_position()
 	if Input.is_action_pressed("lmb"):
-		reposition_with_corners(start, get_global_mouse_position())
+		_reposition_with_corners(start, get_global_mouse_position())
 	
 #endregion
---
+++ pile/pile_element.gd
@@ -0,0 +1,10 @@
+class_name PileElement
+extends Node2D
+
+var movement_constraint := Vector2.RIGHT
+
+
+func reposition(neighbors: Array[Node2D]) -> void:
+	var new_pos: Vector2 = neighbors.reduce(func(sum, x): return sum + x, Vector2.ZERO) / neighbors.size()
+	new_pos *= movement_constraint
+	
